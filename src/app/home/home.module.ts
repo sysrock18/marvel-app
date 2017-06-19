98c402528bf8d2dbbe427fef98144c418fc50f3c
Simon Gonzalez <sgonzalez@galpaservices.com>
2017-06-19 16:17:05 -0400

    Items para personajes y comics favoritos, paginador

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared';
@@ -14,7 +15,8 @@ const homeRouting: ModuleWithProviders = RouterModule.forChild([
 @NgModule({
   imports: [
     homeRouting,
-    SharedModule
+    SharedModule,
+    NgbModule
   ],
   declarations: [
     HomeComponent
